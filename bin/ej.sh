ab68d23896c90695625188970882c144ed733738
monkey999 <34706709+monkey999por@users.noreply.github.com>
2020-10-15 22:10:57 +0900

    stdinから読み込み

--- bin/ej.sh
+++ bin/ej.sh
@@ -4,14 +4,22 @@
 # https://qiita.com/satto_sann/items/be4177360a0bc3691fdf
 # https://script.google.com/home?hl=ja
 function ej () {
- if [ -z "${@}" ]; then
-  echo it has no argument
-  return
- fi
+    if [ -p /dev/stdin ]; then
+        if [ -z "${@}" ]; then 
+            local __str=`cat -`
+        fi
+    else
+        local __str=$@
+    fi
 
- local result=$(curl -L "https://script.google.com/macros/s/AKfycbyWDoCWmfmiGPA7JqGpH9_ec2f90tlIFRnKW2JHEWRfyQtXpahd/exec?text=\"$(echo ${@}| sed "s/ /+/g")\"&source=en&target=ja")
- echo -e "\n"
- echo "${@}"
- echo -e "${result}\n"
+    if [ -z "${__str}" ]; then
+        echo it has no argument
+        return
+    fi
+
+    local result=$(curl -L "https://script.google.com/macros/s/AKfycbyWDoCWmfmiGPA7JqGpH9_ec2f90tlIFRnKW2JHEWRfyQtXpahd/exec?text=\"$(echo ${__str}| sed "s/ /+/g")\"&source=en&target=ja")
+    echo -e "\n"
+    echo "${@}"
+    echo -e "${result}\n"
 }
 
