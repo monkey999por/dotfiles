9137997aebbc872434f310decb47fd7adb1eafc2
monkey999 <34706709+monkey999por@users.noreply.github.com>
2020-10-18 00:26:33 +0900

    dust command

--- bin/dust.sh
+++ bin/dust.sh
@@ -1,7 +1,24 @@
-#!/bin/bash
+ #!/bin/bash
 
 # like a windows dust box
 # if delete file or directory, move dust box directory
 function dust () {
- echo dust
+    if [ -z ${DUSTBOX} ]; then
+        echo not defined '$DUSTBOX'
+        return
+    fi
+
+    if [ -h "${1}" ]; then
+        cp --attributes-only "${1}" "${DUSTBOX}"
+        rm -f "${1}"
+    elif [ -f "${1}" ]; then
+        cp "${1}" "${DUSTBOX}"
+        rm -f "${1}"
+    elif [ -d "${1}" ]; then
+        cp -r "${1}" "${DUSTBOX}"
+        rm -r "${1}"
+    else 
+        echo -e "can't delete this. it is not file or directory or symbolic link.\nuse \"rm\" command."
+    fi
+
 }
