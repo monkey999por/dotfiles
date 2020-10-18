bd4e32581dc597f65b0e7b055c5a650dbcc6880e
monkey999 <34706709+monkey999por@users.noreply.github.com>
2020-10-18 10:22:53 +0900

    dust: bk with date

--- bin/dust.sh
+++ bin/dust.sh
@@ -23,21 +23,24 @@ function dust () {
 
     #バックアップ先ディレクトリ
     backup_path=${DUSTBOX}/$(pwd | sed "s/^\///g" )
-    echo "back up: ${backup_path}"
     mkdir -p ${backup_path}
 
     #削除対象
     local remove_target="$(pwd)/$(basename ${1})"
     echo "remove target: ${remove_target}"
     
+    # back up file name
+    local bk_file_name="$(basename ${remove_target}_$(date "+%Y_%m_%d_%H_%M_%S"))"
+    echo "back up: ${backup_path}/${bk_file_name}"
+
     if [ -h "${1}" ]; then
         cp --attributes-only "${1}" "${DUSTBOX}/"
         rm -f "${1}"
     elif [ -f "${remove_target}" ]; then
-        cp -f "${remove_target}" "${backup_path}/"
+        cp -f "${remove_target}" "${backup_path}/${bk_file_name}"
         rm -f "${remove_target}"
     elif [ -d "${remove_target}" ]; then
-        cp -rf "${remove_target}" "${backup_path}/"
+        cp -rf "${remove_target}" "${backup_path}/${bk_file_name}"
         rm -r "${remove_target}"
     else 
         echo -e "can't delete this. it is not file or directory or symbolic link.\nuse \"rm\" command."
