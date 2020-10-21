c36e55958f9129c58d5cf97d52dd212e6ba24a48
monkey999 <34706709+monkey999por@users.noreply.github.com>
2020-10-21 11:03:49 +0900

    mkdir my dir contitution with no check

--- custom/set_home.sh
+++ custom/set_home.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # make Directory my workspace
-if [ ! -d "${HOME}/Main.d" ]; then
- mkdir "${HOME}/Main.d"
- mkdir "${HOME}/Main.d/Document.d"
- mkdir "${HOME}/Main.d/Develop.d"
- mkdir "${HOME}/Main.d/Downloads.d"
- mkdir "${HOME}/Main.d/DustBox.d"
-fi
+mkdir -v -p "${HOME}/Main.d"
+mkdir -v -p  "${HOME}/Main.d/Document.d"
+mkdir -v -p  "${HOME}/Main.d/Develop.d"
+mkdir -v -p  "${HOME}/Main.d/Downloads.d"
+mkdir -v -p  "${HOME}/Main.d/DustBox.d"
