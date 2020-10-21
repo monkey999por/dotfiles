ab6bd1c6bc6ad699259b964450c1ba711068c2be
monkey999 <34706709+monkey999por@users.noreply.github.com>
2020-10-21 11:27:02 +0900

    install extentions to do Externalize. (and refactoring)

---
+++ custom/extentions_list.sh
@@ -0,0 +1,28 @@
+# comment
+# these extentions are installed when $IS.
+declare -a install_targets=(
+    "tree"
+    "unzip"
+
+    # japanese man page. use 'manj' command. ※manj defined in alias
+    "language-pack-ja"
+    "manpages-ja"
+
+    "manpages-ja-dev"
+    "make"
+    "gcc"
+    #game
+    "cmatrix"
+
+)
+
+# install extentions what is defined in the "install_targets"
+#sudo apt update
+#sudo apt upgrade
+
+for i in ${install_targets[@]} ; do
+    echo $i
+    #sudo apt -y install ${i}
+done
+
+#curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
--- install.sh
+++ install.sh
@@ -1,67 +1,36 @@
 #!/bin/bash
 
-IS_INSTALL_EXTENTIONS=false
+IS_INSTALL_EXTENTIONS=true
 
 SCRIPT_DIR="${HOME}/dotfiles"
 DOTFILES="${SCRIPT_DIR}/dotfiles"
 BACKUP="${SCRIPT_DIR}/bk_defualt/$(date "+%Y_%m_%d_%H_%M_%S")"
+CUSTOM="${SCRIPT_DIR}/custom"
 
-#1. set symbolic link
-# TODO -> バックアップして書き込み権限をなくす(削除もできないようにできる？)
-if [ ! -d ${BACKUP} ]; then
-    mkdir -p ${BACKUP}
-fi
+#1. set symbolic link ~/.XXX -> dotfiles/dotfiles/.XXX
+mkdir -p ${BACKUP}
 
-# TODO -> ディレクトリでもちゃんと動くか確認
 for i in $(ls -Al --format=single-column ${DOTFILES}); do
-
-    #back up
+    #back up to ${BACKUP}
     if [ -f "${HOME}/$i" -o -d "${HOME}/$i" ]; then
-        cp -f "${HOME}/$i" "${BACKUP}"
-        rm "${HOME}/$i"
-
-    elif [ -L "${HOME}/$i" ]; then
-        #元のファイルの場所を検索してバックアップする TODO
-        echo $i with link
+        cp -rf "${HOME}/$i" "${BACKUP}/"
+        rm -r "${HOME}/$i"
     fi
 
     ln -snfv "${DOTFILES}/$i" "${HOME}/$i"
 done
 
 #2. make Directory my workspace(like windows)
-. "${SCRIPT_DIR}/custom/set_home.sh"
+. "${CUSTOM}/set_home.sh"
 
 #3. my custom command
 for c in $(ls -Al --format=single-column ${SCRIPT_DIR}/bin/); do
     . "${SCRIPT_DIR}/bin/$c"
 done
 
-
-
+# install extentions what is defined in the "install_targets"
 if [ ${IS_INSTALL_EXTENTIONS} = "true" ]; then
-    #install tools
-    sudo apt update
-    sudo apt upgrade
-    cat ${SCRIPT_DIR}/custom/extentions_list | while read e
-    do
-        if $(echo "${e}" | grep -q --extended-regexp "[#]") ; then
-            continue
-        fi
-
-        if [ -z "${e}" ]; then
-            continue
-        fi
-        sudo apt -y install ${e}
-    done
-
-    #install extentions
-    #curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+    . "${CUSTOM}/extentions_list.sh"
 else
     echo "skip install extentions command.."
 fi
-
-unset c
-unset e
-unset SCRIPT_DIR
-unset DOTFILES
-unset BACKUP
